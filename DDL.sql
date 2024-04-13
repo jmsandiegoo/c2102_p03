cb6edf7c23e38e2cae9cf6aa697c384d8fec7bc7
jweewee <joshuawee555@gmail.com>
2024-04-13 17:26:26 +0800

    Left Procedure 3(formatting) Procedure 4(whole), Functions(whole)

--- DDL.sql
+++ DDL.sql
@@ -109,6 +109,10 @@ CREATE TABLE Hires (
   ccnum     TEXT  NOT NULL
 );
 
+
+
+
+
 -- test data
 
 INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
@@ -138,21 +142,11 @@ INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
 INSERT INTO Bookings (bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
 (1, '2024-04-10', 7, 'john.doe@example.com', '1111222233334444', '2024-04-01', 'Toyota', 'Corolla', 12345),
 (2, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890),
-(3, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890),
-(4, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890),
-(5, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890),
-(6, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890),
-(7, '2024-05-15', 5, 'jane.doe@example.com', '5555666677778888', '2024-05-10', 'Honda', 'Civic', 67890)
-;
+(3, '2024-06-16', 5, 'jane.doe@example.com', '5555666677778888', '2024-06-09', 'Honda', 'Civic', 67890);
 
 INSERT INTO Assigns (bid, plate) VALUES
 (1, 'SJD1234'),
-(2, 'GHD5678'),
-(3, 'GHD5678'),
-(4, 'GHD5678'),
-(5, 'GHD5678'),
-(6, 'GHD5678'),
-(7, 'GHD5678');
+(2, 'GHD5678');
 
 INSERT INTO Handover (bid, eid) VALUES
 (1, 1),
@@ -167,7 +161,7 @@ INSERT INTO Hires (bid, eid, fromdate, todate, ccnum) VALUES
 (1, 1, '2024-04-10', '2024-04-17', '1111222233334444'),
 (2, 2, '2024-05-15', '2024-05-20', '5555666677778888');
 
--- double booking test
+-- trigger 1 double booking test
 INSERT INTO Hires (bid, eid, fromdate, todate, ccnum) VALUES
 (3, 1, '2024-01-03', '2024-01-05', '1111222233334444'),
 (4, 1, '2024-01-05', '2024-01-06', '1111222233334444'),
@@ -175,3 +169,200 @@ INSERT INTO Hires (bid, eid, fromdate, todate, ccnum) VALUES
 (6, 1, '2024-01-02', '2024-01-06', '1111222233334444'),
 (7, 1, '2024-01-06', '2024-01-07', '1111222233334444');
 
+-- TRIGGER 2. Cars (ie CarDetails) cannot be double-booked test
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street');
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+-- (1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+-- (2, '2024-01-05', 1, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+
+-- (1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+-- (2, '2024-01-04', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+
+-- (1, '2024-01-03', 2 ,'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+-- (2, '2024-01-02', 4 ,'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(2, '2024-01-06', 1, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+
+INSERT INTO Assigns (bid, plate) VALUES
+(1, 'SJD1234'),
+(2, 'SJD1234');
+
+-- TRIGGER 3.  During handover, the employee must be located in the same location the booking is for.
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+-- (1, 'Alice Smith', 80000001, 12345),
+(1, 'Bob Johnson', 80000002, 67890);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890);
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+-- (2, '2024-01-05', 1, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+INSERT INTO Assigns (bid, plate) VALUES
+(1, 'SJD1234');
+-- (2, 'SJD1234');
+INSERT INTO Handover (bid, eid) VALUES
+(1, 1);
+
+-- trigger 4 test. Car from booking must be same booking car model
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+-- (1, 'Alice Smith', 80000001, 12345),
+(1, 'Bob Johnson', 80000002, 67890);
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00),
+('Honda', 'FakeCivic', 5, 200.00, 50.00),
+('FakeHonda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+-- ('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890);
+-- ('SJD1234', 'blue', '2020', 'Honda', 'FakeCivic', 67890);
+('SJD1234', 'blue', '2020', 'FakeHonda', 'Civic', 67890);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+-- (1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'FakeCivic', 67890);
+-- (1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'FakeHonda', 'Civic', 67890);
+INSERT INTO Assigns (bid, plate) VALUES
+(1, 'SJD1234');
+
+-- trigger 5 test. Booking's assigned car same location as booking
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+(1, 'Alice Smith', 80000001, 12345),
+(2, 'Bob Johnson', 80000002, 67890);
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890),
+('SCD1234', 'blue', '2020', 'Honda', 'Civic', 12345);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(2, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 12345);
+INSERT INTO Assigns (bid, plate) VALUES
+-- (1, 'SJD1234'); --correct
+(1, 'SCD1234'); --incorrect
+
+
+-- trigger 6 test. Drivers hired within booking's sdate,edate 
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+(1, 'Alice Smith', 80000001, 12345),
+(2, 'Bob Johnson', 80000002, 67890);
+INSERT INTO Drivers (eid, pdvl) VALUES
+(1, 'PDVL123'),
+(2, 'PDVL456');
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890),
+('SCD1234', 'blue', '2020', 'Honda', 'Civic', 12345);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(2, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 12345);
+INSERT INTO Assigns (bid, plate) VALUES
+(1, 'SJD1234');
+INSERT INTO Hires (bid, eid, fromdate, todate, ccnum) VALUES
+-- (1, 1, '2024-01-03', '2024-01-04', '1111222233334444');
+-- (1, 1, '2024-01-03', '2024-01-20', '5555666677778888');
+(1, 1, '2024-01-02', '2024-01-04', '5555666677778888');
+
+-- procedure 1 test
+CALL add_employees(
+  ARRAY[3, 4, 5],
+  ARRAY['John Doe', 'Jane Smith', 'Steve Brown'],
+  ARRAY[80000003, 80000004, 80000005],
+  ARRAY[12345, 12345, 12345]::integer[],
+  ARRAY['PDVL123', NULL, 'PDVL456']
+);
+
+-- procedure 2 test
+CALL add_car(
+  'Toyota', 'Prius', 5, 200.00, 50.00, 
+  ARRAY['ABC4200', 'ABC4201'], ARRAY['Grey', 'White'], 
+  ARRAY[2021, 2022], ARRAY[12345, 67890]);
+CALL add_car(
+  'Honda', 'Civic', 5, 200.00, 50.00, 
+  ARRAY['ABC4200', 'ABC4201'], ARRAY['Grey', 'White'], 
+  ARRAY[2021, 2022], ARRAY[12345, 67890]);
+
+-- procedure 3 test. Return car
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+(1, 'Alice Smith', 80000001, 67890);
+-- INSERT INTO Drivers (eid, pdvl) VALUES
+-- (1, 'PDVL123');
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('SJD1234', 'blue', '2020', 'Honda', 'Civic', 67890);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+INSERT INTO Assigns (bid, plate) VALUES
+(1, 'SJD1234'); --correct
+INSERT INTO Handover (bid, eid) VALUES
+(1, 1);
+
+CALL return_car (1, 1);
+
+-- procedure 4 test. Auto assign cars to bookings
+-- location, without assigned, brand and model match, no double booking
+INSERT INTO Locations (zip, lname, laddr) VALUES
+(67890, 'Uptown', '456 Suburb Street'),
+(12345, 'Downtown', '123 City Center');
+INSERT INTO Employees (eid, ename, ephone, zip) VALUES
+(1, 'Alice Smith', 80000001, 67890);
+INSERT INTO CarModels (brand, model, capacity, deposit, daily) VALUES
+('Honda', 'Civic', 5, 200.00, 50.00),
+('FakeHonda', 'Civic', 5, 200.00, 50.00);
+INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) VALUES
+('BJD1234', 'blue', '2020', 'Honda', 'Civic', 67890),
+('AJD1234', 'blue', '2020', 'Honda', 'Civic', 67890);
+INSERT INTO Customers (email, dob, address, phone, fsname, lsname) VALUES
+('john.doe@example.com', '1985-07-12', '123 Elm Street', 88888888, 'John', 'Doe'),
+('jane.doe@example.com', '1990-05-24', '456 Oak Avenue', 87777777, 'Jane', 'Doe');
+INSERT INTO Bookings( bid, sdate, days, email, ccnum, bdate, brand, model, zip) VALUES
+(1, '2024-01-03', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(2, '2024-01-03', 20, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(3, '2024-01-05', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(4, '2024-01-07', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890),
+(5, '2024-01-09', 2, 'jane.doe@example.com', '5555666677778888', '2024-01-01', 'Honda', 'Civic', 67890);
+-- INSERT INTO Assigns (bid, plate) VALUES
+-- (2, 'BD1234'),
+-- (3, 'GJD1234');
+CALL auto_assign();
+-- INSERT INTO Assigns (bid, plate) VALUES
+-- (1, 'SJD1234'); --correct
--- P03.sql
+++ P03.sql
@@ -10,11 +10,7 @@ Group #73
 
 /* Write your Trigger Below */
 
--- TRIGGER 1. Drivers cannot be double-booked tested/working: ✅
-CREATE TRIGGER check_driver_not_double_booked
-BEFORE INSERT ON Hires
-FOR EACH ROW EXECUTE FUNCTION check_driver_not_double_booked_func();
-
+-- TRIGGER 1. Drivers cannot be double-booked. tested/working: ✅
 CREATE OR REPLACE FUNCTION check_driver_not_double_booked_func()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -22,7 +18,7 @@ DECLARE
 BEGIN
   -- query and check for existence of driver overlap bookings
   SELECT EXISTS (
-    SELECT 1 FROM Hires h 
+    SELECT 1 FROM Hires h
     WHERE h.eid = NEW.eid
     AND (
       NEW.fromdate BETWEEN h.fromdate AND h.todate
@@ -42,7 +38,7 @@ $$ LANGUAGE plpgsql;
 
 -- TRIGGER 2. Cars (ie CarDetails) cannot be double-booked.
 
-CREATE TRIGGER prevent_car_double_booking
+CREATE TRIGGER prevent_car_double_booking ✅
 BEFORE INSERT ON Assigns
 FOR EACH ROW EXECUTE FUNCTION check_car_not_double_booked_func();
 
@@ -51,10 +47,10 @@ DECLARE
     overlapping_count INT;
 BEGIN
   SELECT COUNT(*) INTO overlapping_count
-  FROM Assigns a, Bookins b
-  WHERE a.bid = b.bid AND a.plate = NEW.plate
-  AND b.sdate <= NEW.todate --checks if START of existing booking(b.sdate) is earlier than END of new assignment(NEW.todate), 
-  AND b.edate >= NEW.fromdate;   --checks if END of existing booking(b.edate) is later than START of new assignment(NEW.fromdate)
+  FROM Assigns a, Bookings b, Bookings new_b
+  WHERE a.bid = b.bid AND a.plate = NEW.plate AND NEW.bid = new_b.bid AND a.plate = NEW.plate
+  AND b.sdate <= new_b.sdate + new_b.days --checks if START of existing booking(b.sdate) is earlier than END of new assignment(new_b.edate), 
+  AND b.sdate + b.days >= new_b.sdate;   --checks if END of existing booking(b.edate) is later than START of new assignment(new_b.sdate)
 
   IF overlapping_count > 0 THEN
         RAISE EXCEPTION 'Car is already booked for another assignment during the specified period'; --decide to raise or fail silently, BOTH OK
@@ -65,14 +61,14 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- TRIGGER 3. During handover, the employee must be located in the same location the booking is for.
-CREATE TRIGGER enforce_employee_location
+CREATE TRIGGER enforce_employee_location ✅
 BEFORE INSERT ON Handover
 FOR EACH ROW EXECUTE FUNCTION check_employee_location();
 
 CREATE OR REPLACE FUNCTION check_employee_location() RETURNS TRIGGER AS $$
 DECLARE 
-  employee_zip INT
-  booking_zip INT;
+    employee_zip INT;
+    booking_zip INT;
 BEGIN
 -- Retrieve zip code of employee
   SELECT zip INTO employee_zip -- select zip as employee_zip?
@@ -86,7 +82,7 @@ BEGIN
 
 --Check if employee's location matches booking's location
   IF employee_zip <> booking_zip THEN
-    RAISE EXCEPTION 'Employee must be located in the same location as the booking;'
+    RAISE EXCEPTION 'Employee must be located in the same location as the booking;';
   END IF;
 
   RETURN NEW;
@@ -94,7 +90,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- TRIGGER 4. The car assigned to the booking must be for the car models for the booking.
-CREATE TRIGGER check_car_model_assignment
+CREATE TRIGGER check_car_model_assignment ✅
 BEFORE INSERT ON Assigns
 FOR EACH ROW EXECUTE FUNCTION check_car_model_assignment_func();
 
@@ -109,7 +105,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
---TRIGGER 5. The car (i.e., CarDetails) assigned to the booking must be parked in the same location as the booking is for.
+--TRIGGER 5. The car (i.e., CarDetails) assigned to the booking must be parked in the same location as the booking is for.✅
 CREATE TRIGGER check_car_location_assignment
 BEFORE INSERT ON Assigns
 FOR EACH ROW EXECUTE FUNCTION check_car_location_assignment_func();
@@ -125,33 +121,31 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- TRIGGER 6. Drivers must be hired within the start date and end date of a booking.
+-- TRIGGER 6. Drivers must be hired within the start date and end date of a booking.✅
 CREATE TRIGGER check_driver_hiring_date
 BEFORE INSERT ON Hires
 FOR EACH ROW EXECUTE FUNCTION check_driver_hiring_date_func();
 
 CREATE OR REPLACE FUNCTION check_driver_hiring_date_func() RETURNS TRIGGER AS $$
+DECLARE
+    booking_start_date DATE; 
+    booking_end_date DATE;  --new.fromdate = 0103, new.todate = 0120
 BEGIN
-  DECLARE
-      booking_start_date DATE;
-      booking_end_date DATE;
-  BEGIN
-      SELECT sdate, sdate + days INTO booking_start_date, booking_end_date
-      FROM Bookings
-      WHERE bid = NEW.bid;
+  SELECT sdate, sdate + days INTO booking_start_date, booking_end_date 
+  FROM Bookings --sdate = 0103, edate = 0105
+  WHERE bid = NEW.bid;
 
   -- Check if the hiring date of the driver overlaps with the booking schedule
-    IF (NEW.fromdate > booking_end_date) OR (NEW.todate < booking_start_date) THEN
-      RAISE EXCEPTION 'Drivers must be hired within the start date and end date of a booking.';
-    END IF;
-  END
+  IF (NEW.todate > booking_end_date) OR (NEW.fromdate < booking_start_date) THEN
+    RAISE EXCEPTION 'Drivers must be hired within the start date and end date of a booking.';
+  END IF;
 
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 
---PROCEDURE 1 Add Employees
+--PROCEDURE 1 Add Employees ✅
 CREATE OR REPLACE PROCEDURE add_employees (
     eids INT[], 
     enames TEXT[], 
@@ -164,18 +158,18 @@ DECLARE
 BEGIN
     FOR i IN 1..array_length(eids, 1)
     LOOP
-        INSERT INTO Employee (eid, ename, ephone, zip) 
+        INSERT INTO Employees (eid, ename, ephone, zip) 
         VALUES (eids[i], enames[i], ephones[i], zips[i]);
 
         IF pdvls[i] IS NOT NULL THEN
-            INSERT INTO Driver (eid, pdvl) 
+            INSERT INTO Drivers (eid, pdvl) 
             VALUES (eids[i], pdvls[i]);
         END IF;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
---PROCEDURE 2 Add Car Model
+--PROCEDURE 2 Add Car Model ✅
 CREATE OR REPLACE PROCEDURE add_car (
     brand TEXT , model TEXT , capacity INT,
     deposit NUMERIC , daily NUMERIC ,
@@ -183,42 +177,44 @@ CREATE OR REPLACE PROCEDURE add_car (
 DECLARE
     i INT;
 BEGIN
-    INSERT INTO CarModel (brand, model, capacity, deposit, daily) 
+    INSERT INTO CarModels (brand, model, capacity, deposit, daily) 
     VALUES (brand, model, capacity, deposit, daily);
 
     FOR i IN 1..array_length(plates, 1)
     LOOP
         INSERT INTO CarDetails (plate, color, pyear, brand, model, zip) 
-        VALUES (plates[i], colors[i], pyears[i], brand, model, zips[i],);
+        VALUES (plates[i], colors[i], pyears[i], brand, model, zips[i]);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
 -- PROCEDURE 3 ## how to calculate cost?
 CREATE OR REPLACE PROCEDURE return_car (
-  bid INT, eid INT
+  p_bid INT, eid INT
 ) AS $$
+DECLARE
   booking_record RECORD;
   car_model_record RECORD;
   cost NUMERIC;
 BEGIN
     -- Retrieve the booking details
-    SELECT INTO booking_record * FROM Bookings WHERE bid = bid;
+    SELECT INTO booking_record * FROM Bookings WHERE Bookings.bid = p_bid;
 
   -- Retrieve the car model details
-    SELECT INTO car_model_record * FROM CarModel WHERE brand = booking_record.brand AND model = booking_record.model;
+    SELECT INTO car_model_record * FROM CarModels WHERE CarModels.brand = booking_record.brand 
+    AND CarModels.model = booking_record.model;
 
     -- Calculate the cost
     cost := (car_model_record.daily * booking_record.days) - car_model_record.deposit;
 
     -- If cost is positive and ccnum is not given, retrieve it from the booking
     IF cost > 0 AND booking_record.ccnum IS NULL THEN
-        booking_record.ccnum := (SELECT ccnum FROM Bookings WHERE bid = bid);
+        booking_record.ccnum := (SELECT ccnum FROM Bookings WHERE Bookings.bid = p_bid);
     END IF;
 
     -- Update the Returned table
-    INSERT INTO Returned (bid, eid, ccnum, cost) 
-    VALUES (bid, eid, booking_record.ccnum, cost);
+    INSERT INTO Returned (p_bid, eid, ccnum, cost) 
+    VALUES (p_bid, eid, booking_record.ccnum, cost);
 END;
 $$ LANGUAGE plpgsql;
 
